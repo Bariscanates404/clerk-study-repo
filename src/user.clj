89367dcc39b45c4d8b23a76243a469e419589de7
bariscanates <baris.ates@layermark.com>
2023-05-13 00:05:27 +0300

    merhaba2

--- src/user.clj
+++ src/user.clj
@@ -11,8 +11,14 @@
   (clerk/clear-cache!)
 
   ;; either call `clerk/show!` explicitly
-  (clerk/show! "notebooks/e01.clj")
-  (clerk/show! "notebooks/e02.clj")
+  (clerk/show! "notebooks/e01_form_submit.clj")
+  (clerk/show! "notebooks/e02_entity_types.clj")
+  (clerk/show! "notebooks/e03-clerk-functionalities.md")
+  (clerk/show! "notebooks/e01_form_submit.clj")
+  (clerk/show! "notebooks/e01_form_submit.clj")
+  (clerk/show! "notebooks/e01_form_submit.clj")
+  (clerk/show! "notebooks/e07_baris_denemeler.clj")
+  (clerk/show! "notebooks/T01.clj")
 
   ;end
   ,)
